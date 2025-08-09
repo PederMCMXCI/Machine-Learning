41beae8fce1a27f36e4f29d277acb18725d9a788
PederMCMXCI <Marco.Pede@gmx.de>
2025-08-09 22:53:06 +0200

    Ordner Vehicle_Detection hinzugefügt

---
+++ Vehicle_Detection/daten_download.py
@@ -0,0 +1,23 @@
+import kagglehub
+import shutil
+import os
+
+# Dataset herunterladen
+path = kagglehub.dataset_download("alkanerturan/vehicledetection")
+
+# Zielverzeichnis
+destination =r"G:\Machine_Learning\PVehicleDetection-YOLOv8"
+
+# Sicherstellen, dass das Zielverzeichnis existiert
+os.makedirs(destination, exist_ok=True)
+
+# Alle heruntergeladenen Dateien in das Ziel kopieren
+for root, dirs, files in os.walk(path):
+    for file in files:
+        src_file = os.path.join(root, file)
+        rel_path = os.path.relpath(src_file, path)
+        dest_file = os.path.join(destination, rel_path)
+        os.makedirs(os.path.dirname(dest_file), exist_ok=True)
+        shutil.copy2(src_file, dest_file)
+
+print("Dateien erfolgreich kopiert nach:", destination)
